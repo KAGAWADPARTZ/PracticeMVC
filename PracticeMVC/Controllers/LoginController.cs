7dc588a18189ed559e4bc8e45427f68fc2bbf430
KAGAWADPARTZ <inocenciodaniel13@gmail.com>
2025-07-07 01:15:43 +0800

    LoginAdded

---
+++ PracticeMVC/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿namespace PracticeMVC.Controllers
+{
+
+    using Microsoft.AspNetCore.Mvc;
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+    
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
