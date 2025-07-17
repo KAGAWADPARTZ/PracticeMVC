8b8c829492947901f7f209163ac751605f1b06aa
KAGAWADPARTZ <inocenciodaniel13@gmail.com>
2025-07-17 23:56:27 +0800

    Design Finish / CRUD for user savings not done

--- MoneyWise/Program.cs
+++ MoneyWise/Program.cs
@@ -74,6 +74,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Login}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
